05dcbc437bbdf2c3e01fd304e88898a3b17a2cc4
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-04-20 19:32:48 +0700

    feat: create user.js repository

---
+++ repository/user.js
@@ -0,0 +1,31 @@
+const { userKeuangan } = require('../model/entity.model')
+
+var objReturn = {
+    error: null,
+    data: null
+}
+
+const createUser = async ({ username, password, name }) => {
+    try {
+        const user = await userKeuangan.create({ username, password, name })
+        return { error: null, data: user };
+    } catch (error) {
+        console.error(error);
+        return { error: error, data: null };
+    }
+}
+
+const findByUsername = async (username) => {
+    try {
+        const user = await userKeuangan.findOne({
+            where : {
+                username
+            }
+        })
+        return { error: null, data: user };
+    } catch (error) {
+        console.error(error);
+        return { error: error, data: null };
+    }
+}
+
