52c3cc4d56a1d81dfb548ecae3aa4cb40791c16c
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-04-21 13:08:21 +0700

    🎈 perf(cors): revisi cors

--- index.js
+++ index.js
@@ -8,27 +8,7 @@ const { sequelize } = require('./utils/database')
 const response = require('./utils/response')
 const { userRouter } = require('./controller/user')
 
-app.use(
-    cors({
-        //array of allowed origins
-        origin: [
-            "*",
-        ],
-        credentials: true,
-        methods: "GET,POST,DELETE,PUT,PATCH",
-        crossDomain: true,
-        allowedHeaders: [
-            "Content-Type",
-            "Authorization",
-            "Accept",
-            "Origin",
-            "X-Requested-With",
-            "X-HTTP-Method-Override",
-        ],
-        preflightContinue: false,
-        optionsSuccessStatus: 204,
-    })
-);
+app.use(cors())
 
 app.use(
     bodyParser.urlencoded({
