371a528c4d964f429d31c9e2f52b1b6eccf2e6f0
adityarizkyramadhan <adityarizky1020@gmail.com>
2023-04-21 12:17:56 +0700

    ✨ feat(user): login controller

--- controller/user.js
+++ controller/user.js
@@ -12,12 +12,30 @@ userRouter.post('/register', async (req, res) => {
     const { username, password, name } = req.body
     const hashPass = bcrypt.hashSync(password, 10)
     try {
-        const data = await userRepository.create({ username, password : hashPass, name })
+        const data = await userRepository.create({ username, password: hashPass, name })
         if (data.error) {
             return response.fail(res, StatusCodes.BAD_REQUEST, data.error)
         }
         const token = jwt.generateToken(data.data.id)
-        return response.success(res, StatusCodes.CREATED, {token : token})
+        return response.success(res, StatusCodes.CREATED, { token: token })
+    } catch (error) {
+        return response.fail(res, StatusCodes.INTERNAL_SERVER_ERROR, "server error")
+    }
+})
+
+userRouter.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    try {
+        const data = await userRepository.findByUsername(username)
+        if (data.error) {
+            return response.fail(res, StatusCodes.BAD_REQUEST, data.error)
+        }
+        const isLogin = bcrypt.compareSync(password, data.data.password)
+        if (!isLogin) {
+            return response.fail(res, StatusCodes.UNAUTHORIZED, "password not match")
+        }
+        const token = jwt.generateToken(data.data.id)
+        return response.success(res, StatusCodes.CREATED, { token: token })
     } catch (error) {
         return response.fail(res, StatusCodes.INTERNAL_SERVER_ERROR, "server error")
     }
